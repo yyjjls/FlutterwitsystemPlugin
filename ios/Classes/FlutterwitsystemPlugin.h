bf5a418493d6a83476082df7b3d42fbc2014324f
yyjjls <635033571@qq.com>
2020-05-04 07:06:44 -0700

    ios修改加密码代码

--- ios/Classes/FlutterwitsystemPlugin.h
+++ ios/Classes/FlutterwitsystemPlugin.h
@@ -1,4 +1,5 @@
 #import <Flutter/Flutter.h>
+#import <CommonCrypto/CommonCrypto.h>
 
 @interface FlutterwitsystemPlugin : NSObject<FlutterPlugin>
 @end
--- ios/Classes/induce/Induce.swift
+++ ios/Classes/induce/Induce.swift
@@ -150,25 +150,31 @@ class Induce:NSObject,CBCentralManagerDelegate,CBPeripheralDelegate{
             let allData = characteristic.value;
             print("读取到的token\(dataToString(data: allData!))");
             //进行加密
-            do {
-              // encrypt
-               // 加密密钥：491b86de5a7258a63df7277760881ded
-                //加密数据：3d734b49014bc2f9d541175d0524ff93
-             //加密好的数据：01559319fb9a10dc4dcfd05a02255d39e9
-              let aes = try AES(keyString: "491b86de5a7258a63df7277760881ded")
-              //let encryptedData = try aes.encrypt(dataToString(data: allData!))
-                let encryptedData = try aes.encrypt("3d734b49014bc2f9d541175d0524ff93")
-
-              // decrypt
-             // let decryptedString = try aes.decrypt(encryptedData)
-              print("加密好的数据\(dataToString(data: encryptedData))") // The black knight always triumphs!
-                print("加密好的数据\(encryptedData.toHexString())")
-
-            } catch {
-              print(error)
-            }
+            //加密密钥：491b86de5a7258a63df7277760881ded
+            //加密数据：3d734b49014bc2f9d541175d0524ff93
+         //加密好的数据：01559319fb9a10dc4dcfd05a02255d39e9
+//            let aes = try Data.init().aes(keyData: Data(hex:"491b86de5a7258a63df7277760881ded"), ivData: allData!, operation: 0)
+            
+            do{
+                let aes = try Data.init().dataCryptAES128(0x0001,
+                                                          0,
+                                                          Data(hex:"491b86de5a7258a63df7277760881ded"),
+                                                          Data(hex:"3d734b49014bc2f9d541175d0524ff93"))
+            print("加密好的数据\(Data(hex: "01\(aes.toHexString())").toHexString())")
+                 print("加密好的数据1111:\(Data(hex:"3d734b49014bc2f9d541175d0524ff93"))")
+            peripheral.writeValue(Data(hex: "01\(aes.toHexString())"), for: unlockCharacteristic!, type: CBCharacteristicWriteType.withResponse)
+            }catch CryptError.noIV{
                 
-//            peripheral.writeValue("hh".data(using:String.Encoding.utf8), for: unlockCharacteristic!, type: CBCharacteristicWriteType.withResponse)
+            }catch CryptError.cryptFailed{
+                
+            }catch CryptError.notConvertTypeToData{
+                
+            }catch{
+                
+            }
+             
+            
+            
         }
     }
     
@@ -200,77 +206,4 @@ class Induce:NSObject,CBCentralManagerDelegate,CBPeripheralDelegate{
 
 
 
-
-extension Data {
-    public init(hex: String) {
-        self.init(bytes: Array<UInt8>(hex: hex))
-    }
-    public var bytes: Array<UInt8> {
-        return Array(self)
-    }
-    public func toHexString() -> String {
-        return bytes.toHexString()
-    }
-}
-
-extension Array {
-    public init(reserveCapacity: Int) {
-        self = Array<Element>()
-        self.reserveCapacity(reserveCapacity)
-    }
-    
-    var slice: ArraySlice<Element> {
-        return self[self.startIndex ..< self.endIndex]
-    }
-}
-
-extension Array where Element == UInt8 {
-    public init(hex: String) {
-        self.init(reserveCapacity: hex.unicodeScalars.lazy.underestimatedCount)
-        var buffer: UInt8?
-        var skip = hex.hasPrefix("0x") ? 2 : 0
-        for char in hex.unicodeScalars.lazy {
-            guard skip == 0 else {
-                skip -= 1
-                continue
-            }
-            guard char.value >= 48 && char.value <= 102 else {
-                removeAll()
-                return
-            }
-            let v: UInt8
-            let c: UInt8 = UInt8(char.value)
-            switch c {
-            case let c where c <= 57:
-                v = c - 48
-            case let c where c >= 65 && c <= 70:
-                v = c - 55
-            case let c where c >= 97:
-                v = c - 87
-            default:
-                removeAll()
-                return
-            }
-            if let b = buffer {
-                append(b << 4 | v)
-                buffer = nil
-            } else {
-                buffer = v
-            }
-        }
-        if let b = buffer {
-            append(b)
-        }
-    }
-    
-    public func toHexString() -> String {
-        return `lazy`.reduce("") {
-            var s = String($1, radix: 16)
-            if s.count == 1 {
-                s = "0" + s
-            }
-            return $0 + s
-        }
-    }
-}
 
