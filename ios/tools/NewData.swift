dedd6453b83cfae79949071793ff6edc206efae2
yyjjls <635033571@qq.com>
2020-05-05 01:32:11 -0700

    wip

---
+++ ios/tools/NewData.swift
@@ -0,0 +1,23 @@
+//
+//  NewData.swift
+//  flutterwitsystem
+//
+//  Created by yyjjls on 2020/5/5.
+//
+
+import Foundation
+
+
+extension Data {
+ 
+
+  public func toHexString() -> String {
+    `lazy`.reduce(into: "") {
+      var s = String($1, radix: 16)
+      if s.count == 1 {
+        s = "0" + s
+      }
+      $0 += s
+    }
+  }
+}
