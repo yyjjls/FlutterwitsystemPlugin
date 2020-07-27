8b6ade35936c62496d2083467d13f9ca1a02ec97
yyjjls <635033571@qq.com>
2020-07-27 22:41:11 +0800

    wip

--- android/src/main/java/com/witsystem/top/flutterwitsystem/smartconfig/SmartConfigManager.java
+++ android/src/main/java/com/witsystem/top/flutterwitsystem/smartconfig/SmartConfigManager.java
@@ -17,7 +17,6 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
 import androidx.core.location.LocationManagerCompat;
 
 import java.net.InetAddress;
